c72dc688975500015bae2b1af1ab5c6f7d2f0160
Gorla Ravi Teja <gorla.ravitejayadav@gmail.com>
2025-09-25 12:19:33 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,7 @@
 import time
 Enter = input("Enter a String:").strip()
-if Enter == Enter[::-1]:
-    print(f"{Enter} is a Palindrome")
-else:
-    print(f"{Enter} is not a Palindrome")
+st = ""
+for i in Enter:
+    st = i + st
+
+print(st)
